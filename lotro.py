c002d57ef5e78bd3638fc76fc6b91a81d6db9aea
pdai006 <baviaannl@hotmail.com>
2019-02-23 14:07:57 +0000

    First version of LotRO class assignment bot.

---
+++ lotro.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import asyncio
+import discord
+import time
+import json
+
+from collections import OrderedDict
+
+testing = False
+
+if not testing:
+    # On boot the pi launches the bot faster than it gets internet access.
+    # Avoid all resulting host not found errors
+    print('Waiting for system to fully boot')
+    time.sleep(10)
+    print('Continuing')
+
+client = discord.Client()
+version = "v1.0.0"
+
+# Load the config file
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+token = config['DEFAULT']['BOT_TOKEN']
+serverid = config['DISCORD']['SERVER_ID']
+
+# These class roles will be automatically created if they don't exist on the server.
+role_names = {
+    'BEORNING': 'Beorning',
+    'BURGLAR': 'Burglar',
+    'CAPTAIN': 'Captain',
+    'CHAMPION': 'Champion',
+    'GUARDIAN': 'Guardian',
+    'HUNTER': 'Hunter',
+    'LOREMASTER': 'Loremaster',
+    'MINSTREL': 'Minstrel',
+    'RUNEKEEPER': 'Runekeeper',
+    'WARDEN': 'Warden'
+}
+
+# The pi runs Python 3.5 where dictionaries aren't ordered yet.
+# Maintain consistency with Python 3.6
+class_roles = OrderedDict()
+emojis = OrderedDict()
+
+# Delete the last 100 messages from the channel.
+# 100 is discord API limit.
+async def clear_channel(channel):
+    async for msg in client.logs_from(channel, limit = 100 ):
+        await client.delete_message(msg)
+
+# Cleans up the channel, adds the initial post, adds reactions and pins the post.
+async def prepare_channel(channel):
+    await clear_channel(channel)
+
+    global post
+    post = await client.send_message(channel, 'Please react with each class you want to sign up for or click \u274C to remove all your class roles.')
+    for key, value in emojis.items():
+        await client.add_reaction(post,value)
+    await client.add_reaction(post,'\u274C')
+    await client.pin_message(post)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(game=discord.Game(name=version))
+
+    server = client.get_server(id=serverid)
+    print('Welcome to {0}'.format(server))
+
+    global class_roles
+    global emojis
+    global command_channel
+
+    # Get the custom emojis.
+    all_emojis = list(client.get_all_emojis())
+
+    # Initialise class roles and create roles if they do not exist yet.
+    for key, value in role_names.items():
+        class_role = discord.utils.get(server.roles, name=value)
+        if class_role is None:
+            class_role = await client.create_role(server, name=value, mentionable=True)
+        class_roles[key] = class_role
+    # Initialise the class emojis in the emoji dictionary.
+    # Assumes class emoji name is equal to class role name.
+    # Quadratic runtime is poor performance for this. // Should rewrite
+    for e in all_emojis:
+        for key, value in role_names.items():
+            if e.name == value:
+                emojis[key] = e
+
+    # Gets the channel that will be used to issue commands by users.
+    # Creates the channel if it does not yet exist.
+    command_channel = discord.utils.get(server.channels, name='class-roles')
+    if command_channel is None:
+        command_channel = await client.create_channel(server, 'class-roles', type=discord.ChannelType.text)
+
+    # Wait a bit to give Discord time to create the channel before we start using it.
+    await asyncio.sleep(1)
+    await prepare_channel(command_channel)
+
+@client.event
+async def on_reaction_add(reaction,user):
+    # Check if the reaction is to the bot's post.
+    if reaction.message.id != post.id:
+        return
+    # Check if the reaction isn't made by the bot.
+    if user == client.user:
+        return
+    # Check if the reaction emoji matches any of our class emojis.
+    for key,value in emojis.items():
+        if reaction.emoji == value:
+            # Add user to the class role. 
+            await client.add_roles(user, class_roles[key])
+            # Send confirmation message.
+            await client.send_message(reaction.message.channel, 'Added {0} to @{1}.'.format(user.mention,class_roles[key]))
+    # Check if the reaction emoji is the cancel emoji
+    if reaction.emoji == '\u274C':
+        # Send a message because this will take 5s.
+        await client.send_message(reaction.message.channel, 'Removing...')
+        # Remove the user from all class roles.
+        for key,value in class_roles.items():
+            await client.remove_roles(user, value)
+            # Discord rate limits requests; drops requests if too fast.
+            await asyncio.sleep(0.5)
+        # Send confirmation message.
+        await client.send_message(reaction.message.channel, 'Removed {0} from all class roles.'.format(user.mention))
+
+@client.event
+async def on_reaction_remove(reaction,user):
+    # Check if the reaction is to the bot's post.
+    if reaction.message.id != post.id:
+        return
+    # Check if the reaction isn't made by the bot.
+    if user == client.user:
+        return
+    # Check if the reaction emoji matches any of our class emojis.
+    for key,value in emojis.items():
+        if reaction.emoji == value:
+            # Remove user from the class role. 
+            await client.remove_roles(user, class_roles[key])
+            # Send confirmation message.
+            await client.send_message(reaction.message.channel, 'Removed {0} from @{1}.'.format(user.mention,class_roles[key]))
+
+# TO DO: Add a command to clear the channel instead of only doing this on boot.
+
+client.run(token)
