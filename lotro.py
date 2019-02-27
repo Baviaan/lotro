668088715f948ce6e935b56c31b1701858338b08
pdai006 <baviaannl@hotmail.com>
2019-02-27 02:05:44 +0000

    Added dwarf cheat sheet

--- lotro.py
+++ lotro.py
@@ -58,7 +58,8 @@ async def prepare_channel(channel):
     await clear_channel(channel,100)
 
     global role_post
-    role_post = await client.send_message(channel, 'Please mute this channel or lose your sanity like me.\n\nReact with each class you want to sign up for or click \u274C to remove all your class roles.\n(You can type `!roles` to check which class roles you currently have.)')
+    message_content = 'Please mute this bot command channel or lose your sanity like me.\n\nReact to this post with each class you want to sign up for or click \u274C to remove all your class roles.\n\n*Further commands that can be used in this channel*:\n`!roles` Shows which class roles you currently have.\n`!dwarves` Shows a list of the 13 dwarves in the Anvil raid with their associated skills. (Work in progress.)'
+    role_post = await client.send_message(channel, message_content)
     for key, value in emojis.items():
         await client.add_reaction(role_post,value)
     await client.add_reaction(role_post,'\u274C')
@@ -77,12 +78,12 @@ async def show_class_roles(user):
     send = send[:-2]
     send = send + '.'
     if has_class_role:
-        await client.send_message(class_role_channel,send)
+        await client.send_message(command_channel,send)
     else:
-        await client.send_message(class_role_channel,'{0} does not have any class roles assigned.'.format(user.mention))
+        await client.send_message(command_channel,'{0} does not have any class roles assigned.'.format(user.mention))
 
 # Process commands for class role channel
-async def class_role_command(message):
+async def command(message):
     # Clear the posts in the channel.
     if message.content.startswith('!clear'):
         # Check if user has sufficient permissions for this command
@@ -90,24 +91,48 @@ async def class_role_command(message):
             # Reset the channel
             await prepare_channel(message.channel)
     # Return class roles for the user.
-    if message.content.startswith('!roles'):
+    elif message.content.startswith('!roles'):
         await show_class_roles(message.author)
+    elif message.content.startswith('!dwarves'):
+        ingor = '**Ingór I the Cruel**: "How much more can your mortal form take?" - Applies 1 stack of incoming healing debuff.\n'
+        oiko2 = '**Óiko II Rill-Seeker**: "I seek the mithril stream." - Ice line\n'
+        dobruz = '**Dóbruz IV the Unheeding**: "You look like a weakling."/"I challenge you." - Picks random target; requires force taunt.\n'
+        mozun = '**Mozun III Wyrmbane**: "I will not abide a worm to live." - Summons worm.\n'
+        kuzek = '**Kúzek Squint-Eye**: TBD - Stand behind him in close range to avoid stun.\n'
+        luvek = '**Lúvek I the Rueful**: TBD - +100% melee damage and crit chance.\n'
+        oiko = '**Óiko I the Bellower**: TBD - TBD \n'
+        kamluz = '**Kamluz II Stoneface**: TBD - TBD \n'
+        dobruz2 = '**Dóbruz II Stark-heart**: "The Zhelruka clan is mine to protect." - Allies take -50% incoming damage, must be interrupted.\n'
+        brantokh2 = '**Brántokh II the Sunderer**: "Cower before the might of the Zhelruka."/"I will bring this mountain down." - 20m AoE.\n'
+        brunek = '**Brúnek I Clovenbow**: "Taste my axes!" - DoT on random person until interrupted.\n'
+        rurek = '**Rúrek VI the Shamed**: "What have I done?"/"I have failed my people." - Bubble on dwarf.\n'
+        brantokh = '**Brántokh I Cracktooth**: "Want to know why they call me cracktooth?" - AoE swipe (low damage)'
+        text = ingor+oiko2+dobruz+mozun+kuzek+luvek+oiko+kamluz+dobruz2+brantokh2+brunek+rurek+brantokh
+        await client.send_message(message.channel,text)
 
 # Process commands for the raid channel
 async def raid_command(message):
     if message.content.startswith('!raid'):
-        arguments = message.content.split(" ",3)
-        if len(arguments) != 4:
-            await client.send_message(message.channel, 'Usage: !raid <name> <tier> <time>')
+        arguments = message.content.split(" ",4)
+        if len(arguments) != 5:
+            await client.send_message(message.channel, 'Usage: !raid <name> <tier> <bosses> <time>\nExample: `!raid Anvil 2 all 4pm server`')
             return
-        arguments[3] = arguments[3].replace('server','EST') # rip when DST arrives
-        time = dateparser.parse(arguments[3],settings={'PREFER_DATES_FROM': 'future'})
+        arguments[4] = arguments[4].replace('server','EST') # rip when DST arrives
+        time = dateparser.parse(arguments[4],settings={'PREFER_DATES_FROM':'future'})
+        if time is None:
+            msg = await client.send_message(message.channel, 'I did not understand the specified time. Please try again.')
+            await asyncio.sleep(20)
+            await client.delete_message(msg)
+            return
         raid = {
-        'NAME': arguments[1].capitalize(), # Should make it default to Anvil
+        'NAME': arguments[1].capitalize(),
         'TIER': arguments[2],
-        'TIME': time # Should make it default to server time
+        'BOSS': arguments[3],
+        'TIME': time
         }
-        await client.send_message(message.channel, '{0} Tier {1} at {2}'.format(raid['NAME'],raid['TIER'],raid['TIME'])) # Should format output
+        embed = discord.Embed(title='{0} T{1} at {2}'.format(raid['NAME'],raid['TIER'],raid['TIME']), colour = discord.Colour(0x3498db), description='Bosses: {0}'.format(raid['BOSS']))
+
+        await client.send_message(message.channel, embed=embed) # Should format output
 
 async def bid_five(message):
     # I wonder what unexpected words this is going to trigger on
@@ -125,7 +150,7 @@ async def on_ready():
 
     global class_roles
     global emojis
-    global class_role_channel
+    global command_channel
     global raid_channel
 
     # Get the custom emojis.
@@ -147,16 +172,16 @@ async def on_ready():
 
     # Get the channels that will be used to issue commands by users.
     # Creates the channel if it does not yet exist.
-    class_role_channel = discord.utils.get(server.channels, name='class-roles')
-    if class_role_channel is None:
-        class_role_channel = await client.create_channel(server, 'class-roles', type=discord.ChannelType.text)
+    command_channel = discord.utils.get(server.channels, name='class-roles')
+    if command_channel is None:
+        command_channel = await client.create_channel(server, 'class-roles', type=discord.ChannelType.text)
     raid_channel = discord.utils.get(server.channels, name='raids')
     if raid_channel is None:
         raid_channel = await client.create_channel(server, 'raids', type=discord.ChannelType.text)
 
     # Wait a bit to give Discord time to create the channel before we start using it.
     await asyncio.sleep(1)
-    await prepare_channel(class_role_channel)
+    await prepare_channel(command_channel)
 
 @client.event
 async def on_reaction_add(reaction,user):
@@ -206,9 +231,9 @@ async def on_message(message):
     # Check if the message isn't sent by the bot.
     if message.author == client.user:
         return
-    # Check if message is sent in class role channel
-    if message.channel == class_role_channel:
-        await class_role_command(message)
+    # Check if message is sent in command channel
+    if message.channel == command_channel:
+        await command(message)
     # Check if message is sent in raid channel
     elif message.channel == raid_channel:
         await raid_command(message)
