29cde2142becf20377e4f8b37628485ba550f2c4
pdai006 <baviaannl@hotmail.com>
2020-05-02 23:13:26 +0100

    exp backoff

--- source/monitor.sh
+++ source/monitor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 date >> ./saruman.log
 echo "starting Saruman.." >> ./saruman.log
+let n=20
 until python3 -u ./main.py >> ./saruman.log 2>> ./saruman.log; do
     date >> ./saruman.log
     echo "Saruman crashed with exit code $?. Respawning.." >> ./saruman.log
-    sleep 10
+    sleep $n
     date >> ./saruman.log
     echo "starting Saruman.." >> ./saruman.log
+    n=$((n*12/10))
 done
