35f0fd00024b5a1bb2a3ad3c305f3ce56c7284ab
pdai006 <baviaannl@hotmail.com>
2020-06-22 00:11:28 +0100

    formatting

--- source/initialise.py
+++ source/initialise.py
@@ -9,7 +9,7 @@ async def initialise(guild, channel, prefix, role_names):
            _("*Further commands that can be used in this channel*:"),
            _("`{0}roles` Shows which class roles you currently have.").format(prefix),
            _("`{0}dwarves` Shows a list of the 13 dwarves in the Anvil raid with their associated skills.").format(prefix),
-           _("`{0}timezone` to set your default timezone to be used for your raid commands."),
+           _("`{0}timezone` to set your default time zone to be used for your raid commands.").format(prefix),
            _("`{0}apply` to apply to {1}.").format(prefix, guild.name),
            _("`{0}help` for further instructions.").format(prefix),
            _("`{0}about` to see the bot info.").format(prefix)]
