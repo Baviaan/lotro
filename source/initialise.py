9748b712241a8463fdc2e9ce63820d1f1208a6e6
pdai006 <baviaannl@hotmail.com>
2020-06-22 00:08:37 +0100

    add new command to summary list

--- source/initialise.py
+++ source/initialise.py
@@ -8,7 +8,8 @@ async def initialise(guild, channel, prefix, role_names):
            _("React to this post with each class role you want to sign up for or click {0} to remove all your class roles.\n").format(cancel_emoji),
            _("*Further commands that can be used in this channel*:"),
            _("`{0}roles` Shows which class roles you currently have.").format(prefix),
-           _("`{0}dwarves` Shows a list of the 13 dwarves in the Anvil raid with their associated skills. (Work in progress.)").format(prefix),
+           _("`{0}dwarves` Shows a list of the 13 dwarves in the Anvil raid with their associated skills.").format(prefix),
+           _("`{0}timezone` to set your default timezone to be used for your raid commands."),
            _("`{0}apply` to apply to {1}.").format(prefix, guild.name),
            _("`{0}help` for further instructions.").format(prefix),
            _("`{0}about` to see the bot info.").format(prefix)]
