e98f796cb1765a61e8e4b87b89d781bc5d6a358e
pdai006 <baviaannl@hotmail.com>
2021-04-08 16:18:19 +0100

    life is hard

--- source/__init__.py
+++ source/__init__.py
@@ -6,4 +6,4 @@ Saruman - A Discord bot for scheduling raids in Standing Stone Games' Lord of th
 
 __author__ = "Baviaan"
 __license__ = "GNU General Public License v3.0"
-__version__ = "4.0.0"
+__version__ = "4.1.0"
