ea59d52ad0c05959990fcef2bbac7957641a3f0d
pdai006 <baviaannl@hotmail.com>
2023-07-04 00:11:34 +0100

    no formatting here

--- source/raid_cog.py
+++ source/raid_cog.py
@@ -205,7 +205,7 @@ class RaidCog(commands.Cog):
     ])
     async def specs_respond(self, interaction: discord.Interaction, classes: Classes, spec: app_commands.Choice[int]):
         if duo_spec and classes.name in duo_spec and (spec.value & 0b100):
-            await interaction.response.send_message(_("Invalid specialization.").format(classes.name))
+            await interaction.response.send_message(_("Invalid specialization."))
             return
         upsert(self.conn, 'Specs', [classes.name], [spec.value], ['player_id'], [interaction.user.id])
         await interaction.response.send_message(_("Updated your {0} specialization.").format(classes.name))
