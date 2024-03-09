d0b4af88ada8c78ac9238b7abd9de9964065d274
pdai006 <baviaannl@hotmail.com>
2024-03-09 14:36:18 +0000

    Adding U39 6man+raid

--- source/__init__.py
+++ source/__init__.py
@@ -6,5 +6,5 @@ Saruman - A Discord bot for scheduling raids in Standing Stone Games' Lord of th
 
 __author__ = "Baviaan"
 __license__ = "GNU General Public License v3.0"
-__version__ = "6.5.1"
-__lotro__ = "38.2.0"
+__version__ = "6.6.0"
+__lotro__ = "38.3.1"
