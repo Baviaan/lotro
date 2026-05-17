a724d0aef8a7599518eb93c3ed213aa976974bef
pdai006 <baviaannl@hotmail.com>
2026-05-17 12:30:02 +0100

    U48.2 data

--- source/__init__.py
+++ source/__init__.py
@@ -6,5 +6,5 @@ Saruman - A Discord bot for scheduling raids in Standing Stone Games' Lord of th
 
 __author__ = "Baviaan"
 __license__ = "GNU General Public License v3.0"
-__version__ = "7.1.2"
-__lotro__ = "48.0.0"
+__version__ = "7.2.0"
+__lotro__ = "48.2.0"
